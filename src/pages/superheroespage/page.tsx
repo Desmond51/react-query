425d17ceff3ac00f1a4c6b3e3623d8f892616875
Desmond51 <desmondtekum9@gmail.com>
2023-08-21 19:32:55 +0100

    fixing issues

--- src/pages/superheroespage/page.tsx
+++ src/pages/superheroespage/page.tsx
@@ -6,22 +6,22 @@ const SuperHeroes = () => {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes').then((res) => {
-      setData(res.data)
-      setIsLoading(false)
-    }, [])
+    axios.get('http://localhost:4000/superheroes').then(response => {
+      setData(response.data)
+      setIsLoading(false);
+    });
 
-    if (isLoading){
-      return <div>Loading....</div>
-    }
-  })
+  }, [])
+  if (isLoading){
+    return <div>Loading....</div>
+  }
   return (
-    <>
+    <div>
       <h2> Super heroes Page</h2>
       {data.map((hero)=> {
         return <div key={hero.name}>{hero.name}</div>
       })}
-    </>
+    </div>
   )
 }
 
