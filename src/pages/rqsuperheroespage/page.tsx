80a8c8c04b53f7253f7a4ff291de3a903a873795
Desmond51 <desmondtekum9@gmail.com>
2023-08-22 16:04:05 +0100

    fetching data with react query

--- src/pages/rqsuperheroespage/page.tsx
+++ src/pages/rqsuperheroespage/page.tsx
@@ -1,7 +1,26 @@
+import axios from 'axios'
 import React from 'react'
+import { useQuery } from 'react-query'
 
+const fetchSuperHeroes = ()=>{
+  return axios.get('http://localhost:4000/superheroes1');
+}
 export default function RQSuperHeroes() {
+  const {isLoading, data, isError, error} = useQuery('super-heroes',fetchSuperHeroes)
+ 
+  if(isLoading){
+    return <div> Loading....</div>
+  }
+  if(isError){
+    return <h2>{error.message}</h2>
+  }
   return (
-    <div>RQ Heroes Page</div>
+    <>
+    <h1>RQ Super Heroes Page</h1>
+    {data?.data.map((hero:any) =>{
+        return <div key={hero.name}>{hero.name}</div>
+      })
+    }
+    </>
   )
 }
--- src/pages/superheroespage/page.tsx
+++ src/pages/superheroespage/page.tsx
@@ -1,24 +1,36 @@
 import axios from 'axios'
+import { error } from 'console'
 import React, { useEffect, useState } from 'react'
 
 const SuperHeroes = () => {
   const [isLoading, setIsLoading]  = useState(true)
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]) as any
+  const [error, setError]= useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes').then(response => {
-      setData(response.data)
+    axios.get('http://localhost:4000/superheroes')
+    .then((res) => {
+      setData(res.data)
       setIsLoading(false);
-    });
+      console.log(data)
+    })
+    .catch((error)=>{
+      setError(error.message)
+      setIsLoading(false)
+    })
 
   }, [])
+
   if (isLoading){
-    return <div>Loading....</div>
+    return <div>Loading...</div>
+  }
+  if(error){
+    return <div>{error}</div>
   }
   return (
     <div>
       <h2> Super heroes Page</h2>
-      {data.map((hero)=> {
+      {data.map((hero:any)=> {
         return <div key={hero.name}>{hero.name}</div>
       })}
     </div>
